327a3941625f2c258b1a2733c83ee48a4124c03c
Michal Kuklis <michal.kuklis@gmail.com>
2012-03-28 14:31:49 -0400

    more work on statable

--- lib/statable.rb
+++ lib/statable.rb
@@ -10,12 +10,14 @@ module Statable
 
       # setup redis-object attribute
       # TODO: add support for different types
-      self.send :counter, attr
+      self.send :value, attr
       #p self.counter(attr)
 
       # wire callbacks
       callbacks.each do |key, callback|
-        self.send key, callback
+        self.send key, -> do
+          result = callback.call
+        end
       end
     end
   end
